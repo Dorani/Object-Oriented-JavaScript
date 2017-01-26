d079046934bffbece6d3b119c5f3de6959164e76
Seif <seif.dorani@gmail.com>
2017-01-26 11:25:05 -0500

    turn method into anonymous function/cleaner

--- code1.js
+++ code1.js
@@ -20,7 +20,7 @@ function diceRoll() {
 //obj lit: we want to incapsulate the function above into a method on the dice
 //to keep all of the state and behavior in 1 place, easy to maintain
 var dice = {
-  roll: function diceRoll() {
+  roll: function() { //anonymous function
     var sides = 6; //sides
     var randonNumber = Math.floor(Math.randon() * sides) + 1; //randon number between 1 and sides
     console.log(randonNumber);
