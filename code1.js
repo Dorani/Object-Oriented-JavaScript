8f743fec21ae038a18b0affb275f056c5f268159
Seif <seif.dorani@gmail.com>
2017-01-26 11:21:40 -0500

    incap function as a method being the value of key roll

--- code1.js
+++ code1.js
@@ -1,3 +1,4 @@
+//obj example
 var person = {
   name: "Lauren",
   generalAssemblySchool: true,
@@ -8,3 +9,20 @@ person.name;
 person.generalAssemblySchool;
 
 //keys are essentially strings
+
+//create a dice roll function, then a dice object
+
+function diceRoll() {
+  var sides = 6; //sides
+  var randonNumber = Math.floor(Math.randon() * sides) + 1; //randon number between 1 and sides
+  console.log(randonNumber);
+}
+//obj lit: we want to incapsulate the function above into a method on the dice
+//to keep all of the state and behavior in 1 place, easy to maintain
+var dice = {
+  roll: function diceRoll() {
+    var sides = 6; //sides
+    var randonNumber = Math.floor(Math.randon() * sides) + 1; //randon number between 1 and sides
+    console.log(randonNumber);
+  }
+}
